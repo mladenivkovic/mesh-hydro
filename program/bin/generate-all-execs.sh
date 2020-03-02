56a552cdd4328d138ac0e366cc7ddf687bbd02ee
Mladen Ivkovic <mladen.ivkovic@hotmail.com>
2020-03-02 21:27:44 +0100

    found better way to create_all_execs

--- program/bin/generate-all-execs.sh
+++ program/bin/generate-all-execs.sh
@@ -28,43 +28,73 @@ genmakefile(){
 }
 
 
-make -f Makefile-individual-execnames clean
+make clean
+make -f Makefile-Riemann clean
 
 
-#---------------------------
-# Vanilla advection
-#---------------------------
-for ndim in 1 2; do
-    for solver in ADVECTION_PWLIN ADVECTION_PWCONST; do
+function advection(){
 
-        make -f Makefile-individual-execnames clean
-        genmakefile $ndim $solver NONE NONE hydro-$solver-"$ndim"D
-        make -f Makefile-individual-execnames
+    #---------------------------
+    # Vanilla advection
+    #---------------------------
+    for ndim in 1 2; do
+        for solver in ADVECTION_PWLIN ADVECTION_PWCONST; do
+            make clean
+            genmakefile $ndim $solver NONE NONE hydro-$solver-"$ndim"D
+            make
+        done
     done
-done
 
 
 
-#---------------------------
-# slope limiters
-#---------------------------
-for ndim in 1 2; do
-    for LIMITER in MINMOD SUPERBEE MC VANLEER; do
-
-        make -f Makefile-individual-execnames clean
-        genmakefile $ndim ADVECTION_PWLIN NONE $LIMITER hydro-advection-"$LIMITER"-"$ndim"D
-        make -f Makefile-individual-execnames
+    #---------------------------
+    # slope limiters
+    #---------------------------
+    for ndim in 1 2; do
+        for LIMITER in MINMOD SUPERBEE MC VANLEER; do
+            make clean
+            genmakefile $ndim ADVECTION_PWLIN NONE $LIMITER hydro-advection-"$LIMITER"-"$ndim"D
+            make
+        done
     done
-done
+    rm -f defines.mk
+}
 
 
-#---------------------------
-# Godunov
-#---------------------------
-for ndim in 1 2; do
-    for RIEMANN in EXACT TRRS TSRS HLLC; do
-        make -f Makefile-individual-execnames clean
-        genmakefile $ndim GODUNOV $RIEMANN NONE hydro-godunov-"$RIEMANN"-"$ndim"D
-        make -f Makefile-individual-execnames
+
+function godunov(){
+
+    #---------------------------
+    # Godunov
+    #---------------------------
+    for ndim in 1 2; do
+        for RIEMANN in EXACT TRRS TSRS HLLC; do
+            make clean
+            genmakefile $ndim GODUNOV $RIEMANN NONE hydro-godunov-"$RIEMANN"-"$ndim"D
+            make
+        done
     done
-done
+    rm -f defines.mk
+}
+
+
+
+function riemann(){
+
+    #---------------------------
+    # Code as a Riemann Solver
+    #---------------------------
+
+    for RIEMANN in EXACT TRRS TSRS; do
+        make -f Makefile-Riemann clean
+        genmakefile 1 NONE $RIEMANN NONE riemann-"$RIEMANN"
+        make -f Makefile-Riemann
+    done
+    rm -f defines.mk
+}
+
+
+
+advection
+godunov
+riemann
